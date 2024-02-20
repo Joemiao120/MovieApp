e29b2134fbcf0c6725a204d96a27b6c7a08e741d
Joe Miao <joe@journey-digital.com>
2024-02-20 21:51:01 +1300

    Finish Detail screen

--- app/src/main/java/com/example/movieapp/screens/detail/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/detail/DetailsScreen.kt
@@ -1,13 +1,21 @@
 package com.example.movieapp.screens.detail
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,12 +28,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
+import com.example.movieapp.widgets.MovieRow
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailsScreen(navController: NavController, movie: String?) {
+fun DetailsScreen(navController: NavController, movieId: String?) {
+
+
+    val movies = getMovies().filter { movie: Movie -> movie.id == movieId }
 
     Scaffold(
         topBar = {
@@ -59,10 +75,27 @@ fun DetailsScreen(navController: NavController, movie: String?) {
         ) {
 
             Column(
-                verticalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = movie.toString(), style = MaterialTheme.typography.bodyMedium)
+                MovieRow(movies.first())
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                LazyRow {
+                    items(movies.first().images) { image ->
+                        Card(
+                            modifier = Modifier
+                                .padding(12.dp)
+                                .size(240.dp)
+                        ) {
+                            Image(
+                                painter = rememberImagePainter(data = image),
+                                contentDescription = ""
+                            )
+                        }
+                    }
+                }
             }
 
         }
--- app/src/main/java/com/example/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/example/movieapp/widgets/widgets.kt
@@ -1,27 +1,42 @@
 package com.example.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
@@ -35,12 +50,15 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable {
                 onItemClick.invoke(movie.id)
             },
         shape = RoundedCornerShape(corner = CornerSize(16.dp))
     ) {
+        var expanded by remember {
+            mutableStateOf(false)
+        }
+
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
@@ -64,19 +82,50 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                 )
 
                 Image(painter = painter, contentDescription = "")
-
-//                Icon(
-//                    imageVector = Icons.Default.AccountBox,
-//                    contentDescription = ""
-//                )
             }
             Column(modifier = Modifier.padding(4.dp)) {
-                Text(text = movie.title,
-                    style = MaterialTheme.typography.titleLarge)
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.titleLarge
+                )
                 Text(text = "Director: ${movie.director}")
                 Text(text = "Released: ${movie.year}")
-            }
 
+
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray, fontSize = 13.sp,
+                                    fontWeight = FontWeight.Light
+                                )
+                            ) {
+                                append("${movie.plot}")
+                            }
+                        }, modifier = Modifier.padding(6.dp))
+                        Divider()
+
+                        Text(text = "Director: ${movie.director}")
+                        Text(text = "Actors: ${movie.actors}")
+                        Text(text = "Rating: ${movie.rating}")
+                    }
+
+                }
+
+                Icon(
+                    imageVector = if (!expanded) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp,
+                    contentDescription = "Arrow Down",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        }
+                )
+            }
         }
     }
 }
